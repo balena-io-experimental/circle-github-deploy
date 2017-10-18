8447c410bfad67171205e2dc564b41192a3f5381
majorz <zahari@resin.io>
2017-10-18 09:25:45 +0300

    List CircleCI build numbers

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,36 @@
 
 set -u
 
+trap "exit 1" TERM
+export TOP_PID=$$
+
 : "${APPNAME:=deploy}"
+: "${CIRCLE_FULL_ENDPOINT:=https://circleci.com/api/v1.1/project/github/$GITHUB_USER/$GITHUB_REPO}"
+: "${CIRCLE_TAG:=}"
 
 main() {
     need_cmd curl
     need_cmd jq
 
-    say 'Hello'
+    if [ -z "$CIRCLE_TAG" ]; then
+        quit "Deploying only when CIRCLE_TAG is defined"
+    fi
+
+    # Get the list with CircleCI build numbers for the current tagged release
+    local _builds=$(ensure circle "$CIRCLE_FULL_ENDPOINT")
+    local _filter='.[] | select(.vcs_tag == "'$CIRCLE_TAG'" and .workflows.job_name != "deploy") | .build_num'
+    local _build_nums=$(ensure jq "$_filter" <<< $_builds)
+
+    IFS=$'\n'
+    for build_num in $_build_nums; do
+        say $build_num
+    done
+}
+
+circle() {
+    ensure curl "${1}?circle-token=$CIRCLE_TOKEN" \
+        -s --retry 3 \
+        -H "Accept: application/json"
 }
 
 say() {
@@ -17,7 +40,12 @@ say() {
 
 err() {
     printf '\33[1;31m%s:\33[0m %s\n' "$APPNAME" "$1" >&2
-    exit 1
+    kill -s TERM $TOP_PID
+}
+
+quit() {
+    say "$1"
+    kill -s TERM $TOP_PID
 }
 
 need_cmd() {
